0a251a4627aa911118ce12fe069331484aa728c0
ZetyaWyn <amruthv780@gmail.com>
2026-09-20 00:42:30 +0530

    dolby: Update Vietnamese translations

--- src/org/lunaris/dolby/ui/components/CreditsDialog.kt
+++ src/org/lunaris/dolby/ui/components/CreditsDialog.kt
@@ -121,7 +121,8 @@ fun CreditsDialog(
         translation("Traditional Chinese", t("DenlNister", "nnn950711")),
         translation("Turkish", t("Ümit Taylan", "jinetty")),
         translation("Russian", t("Dmitry", "dkpost3")),
-        translation("Persian", t("Arman Altafi", "Arman-ATI"))
+        translation("Persian", t("Arman Altafi", "Arman-ATI")),
+        translation("Vietnamese", t("ZetyaWyn"))
     )
     
     Dialog(
